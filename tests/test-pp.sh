8e16b50868d992b1581683c522c5a06962579d1a
David Goodwin <david@palepurple.co.uk>
2014-02-24 18:26:11 +0000

    improve logging (try and re-use postfix queue_id which makes life easier when tracing things);

--- tests/test-pp.sh
+++ tests/test-pp.sh
@@ -2,5 +2,6 @@ echo "helo_name=mail.palepurple.co.uk
 recipient=test@gmail.com
 sender=test@palepurple.co.uk
 client_address=81.133.46.190
+queue_id=RahRharha
 request=smtpd_access_policy
 " | perl -I ../src/ -w ../src/policyd.pl
